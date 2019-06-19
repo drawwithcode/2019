783c600a3af4da0b4606a8ac1e44afa45922f81c
andrea <benedetti.93@gmail.com>
2019-06-19 11:55:04 +0200

    switched from jQuery to pure p5.js, added CRT monitor effect

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
-let home = ["1", "0", "0"];
-let lectures = ["1", "0", "1"];
+let homeIndex = ["1", "0", "0"];
+let lecturesIndex = ["1", "0", "1"];
+let assignmentsIndex = ["2", "0", "1"];
 let code = [];
 let page = location.hash;
+
+let home;
+let lectures;
+let assignments;
 
 function preload(){
   // put preload code here
 }
 
 function setup() {
+  home = select("#home");
+  lectures = select("#lectures");
+  assignments = select("#assignments");
 
   createCanvas(windowWidth,windowHeight);
   frameRate(30);
@@ -15,12 +23,13 @@ function setup() {
   background(0);
   colorMode(RGB, 8);
 
-  print(lectures);
-
-  $("#assignments").hide();
+  lectures.hide();
+  assignments.hide();
 }
 
 function draw() {
+  // ciao.hide();
+
   for (var x = 0; x < width/2; x += 6) {
 
     for (var y = 0; y < height/2; y += 6){
@@ -42,25 +51,39 @@ function keyTyped() {
   $(".channel").html(code);
   print(code);
 
-  if ( JSON.stringify(code) == JSON.stringify(home) ) {
+  if ( JSON.stringify(code) == JSON.stringify(homeIndex) ) {
     history.pushState(null, null, '#');
     print("HOME");
     code = [];
     print(code);
 
-    $("#assignments").hide();
-    $("#home").show();
+    home.show();
+    lectures.hide();
+    assignments.hide();
     return true;
   }
 
-  if ( JSON.stringify(code) == JSON.stringify(lectures) ) {
+  if ( JSON.stringify(code) == JSON.stringify(lecturesIndex) ) {
     history.pushState(null, null, '#lectures');
     print("LECTURES");
     code = [];
     print(code);
 
-    $("#home").hide();
-    $("#assignments").show();
+    home.hide();
+    lectures.show();
+    assignments.hide();
+    return true;
+  }
+
+  if ( JSON.stringify(code) == JSON.stringify(assignmentsIndex) ) {
+    history.pushState(null, null, '#assignments');
+    print("ASSIGNMENTS");
+    code = [];
+    print(code);
+
+    home.hide();
+    lectures.hide();
+    assignments.show();
     return true;
   }
 
